1da778894eb6c652d6df33670c73e3d36dace06a
Dmytry Lavrov <dmytryl@gmail.com>
2026-06-06 18:13:00 -0500

    Set probe z offset to current saved value on the printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1797,7 +1797,7 @@
  *     |           |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40.0, 0, -2.22 }// 0.827
+#define NOZZLE_TO_PROBE_OFFSET { 40.0, 0, -2.55 }// 0.827
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
